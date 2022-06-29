ef8690207c6d3b0344cb4a9d0d432a90c4534b4a
din ezra <dinoezra@gmail.com>
2022-06-29 11:39:46 +0300

    Add a tag 'g' so we can identify a URL

--- WebSearcher/component_classifier.py
+++ WebSearcher/component_classifier.py
@@ -57,7 +57,7 @@ def classify_type(cmpt, cmpt_type='unknown'):
     hybrid = cmpt.find('div', {'class':'ifM9O'})
     twitter = cmpt.find_previous().text == "Twitter Results"
 
-    if 'class' in cmpt.attrs.keys() and cmpt.attrs['class'][0] == 'hlcw0c':
+    if 'class' in cmpt.attrs.keys() and (cmpt.attrs['class'][0] == 'hlcw0c'  or cmpt.attrs['class'][0] == 'g'):
         cmpt_type='general'
     
     # Checks a g-scrolling-carousel for a specific id to classify as top stories as not all
